c6a63b008eece2ffd10587869aa68316e95152d8
Kiya927 <outerest25@gmail.com>
2024-11-13 00:59:35 +0900

    11/12 履歴実装中 sql簡素化中

--- src/main/java/com/jobportal/JobPortal/Service/MainService.java
+++ src/main/java/com/jobportal/JobPortal/Service/MainService.java
@@ -20,43 +20,128 @@ public class MainService {
 
     private final MainRepository repository;
 
-
     //OA作成
     @Transactional
-    public void createOA(OAMainEntity entity){
-        repository.insertMainOA(entity);
+    public void createOA(OAMainEntity entity){repository.insertMainOA(entity);
     }
-    //日付登録
     @Transactional
-    public void createOADates(List<OADatesEntity> entity, Integer officialAbsenceId){repository.insertOADates(entity, officialAbsenceId);}
-    //該当生徒OAList取得
-    public List<OAListEntity> findAllOAs(Integer studentId){
-        return repository.selectAll(studentId);
+    public void createOADates(List<OADatesEntity> entity, Integer officialAbsenceId){
+        repository.insertOADates(entity, officialAbsenceId);
     }
-    public List<OAListEntity> teacherFindAllOAs(){
-        return repository.teacherFindAllOAs();
+    @Transactional
+    public void createJobSearch(JobSearchEntity jobSearchEntity) {repository.insertJobSearch(jobSearchEntity);
     }
-    //DateInfo取得
-    public List<OADateInfoEntity> findDateInfo(Integer oaId) {return repository.selectInfo(oaId);}
-    //就活情報登録
     @Transactional
-    public void createJobSearch(JobSearchEntity jobSearchEntity) {repository.insertJobSearch(jobSearchEntity);}
-    //セミナー情報登録
+    public void createSeminar(SeminarEntity seminarEntity) {repository.insertSeminar(seminarEntity);
+    }
     @Transactional
-    public void createSeminar(SeminarEntity seminarEntity) {repository.insertSeminar(seminarEntity);}
-    //忌引情報登録
+    public void createBereavement(BereavementEntity bereavementEntity) {
+        repository.insertBereavement(bereavementEntity);
+    }
     @Transactional
-    public void createBereavement(BereavementEntity bereavementEntity) {repository.insertBereavement(bereavementEntity);}
-    //出席停止情報登録
+    public void createAttendanceBan(AttendanceBanEntity attendanceBanEntity) {
+        repository.insertAttendanceBan(attendanceBanEntity);
+    }
     @Transactional
-    public void createAttendanceBan(AttendanceBanEntity attendanceBanEntity) {repository.insertAttendanceBan(attendanceBanEntity);}
-    //その他情報登録
+    public void createOther(OtherEntity otherEntity) {repository.insertOther(otherEntity);
+    }
     @Transactional
-    public void createOther(OtherEntity otherEntity) {repository.insertOther(otherEntity);}
-    //提出日情報登録
+    public void createSubmitted(Integer officialAbsenceId) {
+        repository.createSubmittedDate(officialAbsenceId,LocalDate.now());
+    }
+    //List取得
+    public List<OAListEntity> findAllOAs(Integer studentId){return repository.selectAll(studentId);
+    }
+    public List<OAListEntity> teacherFindAllOAs(){return repository.teacherFindAllOAs();
+    }
+    //info取得
+    public OAMainInfoEntity findMainInfo(Integer oaId) {return  repository.selectMainInfo(oaId);
+    }
+    public List<OADateInfoEntity> findDateInfo(Integer oaId) {return repository.selectDateInfo(oaId);
+    }
+    public JobSearchEntity findJobSearchInfo(Integer oaId) {return repository.selectJobSearchInfo(oaId);
+    }
+    public SeminarEntity findSeminarInfo(Integer oaId) {return repository.selectSeminarInfo(oaId);
+    }
+    public BereavementEntity findBereavementInfo(Integer oaId) {return repository.selectBereavementInfo(oaId);
+    }
+    public AttendanceBanEntity findAttendanceBanInfo(Integer oaId) {return repository.selectAttendanceBanInfo(oaId);
+    }
+    public OtherEntity findOtherInfo(Integer oaId) {return repository.selectOtherInfo(oaId);
+    }
+    //過去versionInfo取得
+    public OAMainInfoEntity findMainInfoByVersion(Integer oaId, Integer version) {return  repository.selectMainInfoByVersion(oaId, version);
+    }
+    public List<OADateInfoEntity> selectInfoByVersion(Integer oaId, Integer version) {return repository.selectDateInfoByVersion(oaId, version);
+    }
+    public JobSearchEntity findJobSearchInfoByVersion(Integer oaId, Integer version) {return repository.selectJobSearchInfoByVersion(oaId, version);
+    }
+    public SeminarEntity findSeminarInfoByVersion(Integer oaId, Integer version) {return repository.selectSeminarInfoByVersion(oaId, version);
+    }
+    public BereavementEntity findBereavementInfoByVersion(Integer oaId, Integer version) {return repository.selectBereavementInfoByVersion(oaId, version);
+    }
+    public AttendanceBanEntity findAttendanceBanInfoByVersion(Integer oaId, Integer version) {return repository.selectAttendanceBanInfoByVersion(oaId, version);
+    }
+    public OtherEntity findOtherInfoByVersion(Integer oaId, Integer version) {return repository.selectOtherInfoByVersion(oaId, version);
+    }
+    //削除
     @Transactional
-    public void createSubmitted(Integer officialAbsenceId) {repository.createSubmittedDate(officialAbsenceId,LocalDate.now());}
-
+    public void deleteDate(Integer OAId) {repository.deleteDate(OAId);
+    }
+    @Transactional
+    public void deleteMain(Integer OAId) {repository.deleteMain(OAId);
+    }
+    @Transactional
+    public void deleteJobSearch(Integer OAId) {repository.deleteJobSearch(OAId);
+    }
+    @Transactional
+    public void deleteSeminar(Integer OAId) {repository.deleteSeminar(OAId);
+    }
+    @Transactional
+    public void deleteBereavement(Integer OAId) {repository.deleteBereavement(OAId);
+    }
+    @Transactional
+    public void deleteAttendanceBan(Integer OAId) {repository.deleteAttendanceBan(OAId);
+    }
+    @Transactional
+    public void deleteOther(Integer OAId) {repository.deleteOther(OAId);
+    }
+    @Transactional
+    public void deleteSubmittedDate(Integer OAId) {repository.deleteSubmittedDate(OAId);
+    }
+    //再提出
+    @Transactional
+    public void updateSubmittedDate(Integer OAId) {repository.updateSubmittedDate(OAId, LocalDate.now());
+    }
+    @Transactional
+    public void updateOADates(List<OADatesEntity> dateList, Integer OAId) {repository.updateOADates(dateList, OAId);
+    }
+    @Transactional
+    public void updateJobSearch(JobSearchEntity jobEntity) {repository.updateJobSearch(jobEntity);
+    }
+    @Transactional
+    public void updateSeminar(SeminarEntity seminar) {repository.updateSeminar(seminar);
+    }
+    @Transactional
+    public void updateBereavement(BereavementEntity bereavement) {repository.updateBereavement(bereavement);
+    }
+    @Transactional
+    public void updateAttendanceBan(AttendanceBanEntity attendanceBan) {
+        repository.updateAttendanceBan(attendanceBan);
+    }
+    @Transactional
+    public void updateOther(OtherEntity other) {repository.updateOther(other);
+    }
+    //ステータス更新
+    @Transactional
+    public void updateOAStatus(Integer OAId, String status) {repository.updateOAStatus(OAId, status);
+    }
+    @Transactional
+    public void updateReportRequired(Integer OAId, boolean flag) {repository.updateReportRequired(OAId, flag);
+    }
+    @Transactional
+    public void updateCheck(Integer OAId, String type, boolean check) {repository.updateCheck(OAId, type, check);
+    }
 
     //重複データを排除するために、ListをMapにするメソッド
     public Map<String, List<OALessonsDTO>> toLessonInfoDTO(List<OADateInfoEntity> allInfoDTO) {
@@ -78,46 +163,6 @@ public class MainService {
 //            v.forEach(System.out::println);
 //        });
         return map;
-    }
-
-    public static String dateFormat(LocalDate date) {
-        String yyyy = date.toString().substring(0,4);
-        String mm = date.toString().substring(5,7);
-        String dd = date.toString().substring(8,10);
-        String dow = "";
-        Calendar cal = Calendar.getInstance();
-        cal.set(parseInt(yyyy), parseInt(mm) - 1, parseInt(dd));
-        dow = switch (cal.get(Calendar.DAY_OF_WEEK)) {
-            case Calendar.SUNDAY -> "(日)";
-            case Calendar.MONDAY -> "(月)";
-            case Calendar.TUESDAY -> "(火)";
-            case Calendar.WEDNESDAY -> "(水)";
-            case Calendar.THURSDAY -> "(木)";
-            case Calendar.FRIDAY -> "(金)";
-            case Calendar.SATURDAY -> "(土)";
-            default -> dow;
-        };
-        return yyyy + "年" + mm + "月" + dd + "日" + dow;
-    }
-
-    public OAMainInfoEntity findMainInfo(Integer oaId) {
-        return  repository.selectMainInfo(oaId);
-    }
-
-    public JobSearchEntity findJobSearchInfo(Integer oaId) {
-        return repository.selectJobSearchInfo(oaId);
-    }
-    public SeminarEntity findSeminarInfo(Integer oaId) {
-        return repository.selectSeminarInfo(oaId);
-    }
-    public BereavementEntity findBereavementInfo(Integer oaId) {
-        return repository.selectBereavementInfo(oaId);
-    }
-    public AttendanceBanEntity findAttendanceBanInfo(Integer oaId) {
-        return repository.selectAttendanceBanInfo(oaId);
-    }
-    public OtherEntity findOtherInfo(Integer oaId) {
-        return repository.selectOtherInfo(oaId);
     }
     //公欠授業をリスト化
     public List<OAListDTO> toListEntity(List<OAListEntity> listEntity) {
@@ -162,7 +207,27 @@ public class MainService {
         ));
         return listDTO;
     }
-
+    //yyyy-mm-ddをyyyy年mm月dd日(曜日)にする
+    public static String dateFormat(LocalDate date) {
+        String yyyy = date.toString().substring(0,4);
+        String mm = date.toString().substring(5,7);
+        String dd = date.toString().substring(8,10);
+        String dow = "";
+        Calendar cal = Calendar.getInstance();
+        cal.set(parseInt(yyyy), parseInt(mm) - 1, parseInt(dd));
+        dow = switch (cal.get(Calendar.DAY_OF_WEEK)) {
+            case Calendar.SUNDAY -> "(日)";
+            case Calendar.MONDAY -> "(月)";
+            case Calendar.TUESDAY -> "(火)";
+            case Calendar.WEDNESDAY -> "(水)";
+            case Calendar.THURSDAY -> "(木)";
+            case Calendar.FRIDAY -> "(金)";
+            case Calendar.SATURDAY -> "(土)";
+            default -> dow;
+        };
+        return yyyy + "年" + mm + "月" + dd + "日" + dow;
+    }
+    //statusを日本語化
     public static String existsReport(OAStatus status) {
         if(status == null){
             return "未提出";
@@ -170,15 +235,6 @@ public class MainService {
             return status.getJapaneseName();
         }
     }
-
-    public void updateOAStatus(Integer OAId, String status) {
-        repository.updateOAStatus(OAId, status);
-    }
-
-    public void updateReportRequired(Integer OAId, boolean flag) {
-        repository.updateReportRequired(OAId, flag);
-    }
-
     public OAMainForm toJobSearchForm(OAMainInfoDTO mainInfoDTO, Map<String, List<OALessonsDTO>> lessonInfoEntities, JobSearchEntity jobSearch) {
         Map<String, List<String>> map = new HashMap<>();
         lessonInfoEntities.forEach((k,v)->{
@@ -278,31 +334,6 @@ public class MainService {
                 other.remarks()
         );
     }
-
-
-    //削除
-    public void deleteDate(Integer OAId) {repository.deleteDate(OAId);}
-    public void deleteMain(Integer OAId) {repository.deleteMain(OAId);}
-    public void deleteJobSearch(Integer OAId) {repository.deleteJobSearch(OAId);}
-    public void deleteSeminar(Integer OAId) {repository.deleteSeminar(OAId);}
-    public void deleteBereavement(Integer OAId) {repository.deleteBereavement(OAId);}
-    public void deleteAttendanceBan(Integer OAId) {repository.deleteAttendanceBan(OAId);}
-    public void deleteOther(Integer OAId) {repository.deleteOther(OAId);}
-    public void deleteSubmittedDate(Integer OAId) {repository.deleteSubmittedDate(OAId);}
-    //再提出
-    public void updateSubmittedDate(Integer OAId) {repository.updateSubmittedDate(OAId, LocalDate.now());}
-    public void updateOADates(List<OADatesEntity> dateList, Integer OAId) {repository.updateOADates(dateList, OAId);}
-    public void updateJobSearch(JobSearchEntity jobEntity) {repository.updateJobSearch(jobEntity);}
-    public void updateSeminar(SeminarEntity seminar) {repository.updateSeminar(seminar);}
-    public void updateBereavement(BereavementEntity bereavement) {repository.updateBereavement(bereavement);}
-    public void updateAttendanceBan(AttendanceBanEntity attendanceBan) {repository.updateAttendanceBan(attendanceBan);}
-    public void updateOther(OtherEntity other) {repository.updateOther(other);}
-
-    public void updateCheck(Integer OAId, String type, boolean check) {
-        repository.updateCheck(OAId, type, check);
-    }
-
-
     public boolean checkConditionJudge(Integer OAId, boolean type) {
         if(type){
             return repository.teacherCheckCondition(OAId) && repository.careerCheckCondition(OAId);
