54279043874ac08352a487ccfc2bb701083366ed
Kiya665 <40104kk@saisen.ac.jp>
2025-01-30 12:14:34 +0900

    1/30 アコーディオン実装中

--- src/main/java/com/jobportal/JobPortal/Interceptor/StudentInterceptor.java
+++ src/main/java/com/jobportal/JobPortal/Interceptor/StudentInterceptor.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.util.Map;
+
 //@RequestMapping("/jobportal")
 @RequiredArgsConstructor
 public class StudentInterceptor implements HandlerInterceptor {
@@ -20,27 +22,27 @@ public class StudentInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("studentPreHandle");
         //localでテスト用
-        Student student = new Student();
-        student.setGno(40104);
-        student.setSurname("YourSurName");
-        student.setGivenname("YourGivenName");
-        student.setGroup("生徒");
-        student.setGrade(2);
-        student.setClassroom("B");
-        student.setCno(99);
-        student.setDepartment("YourDepartment");
-        student.setCourse("中級プログラマ");
-        student.setMail("40104kk@saisen.ac.jp");
+//        Student student = new Student();
+//        student.setGno(40104);
+//        student.setSurname("YourSurName");
+//        student.setGivenname("YourGivenName");
+//        student.setGroup("生徒");
+//        student.setGrade(2);
+//        student.setClassroom("A");
+//        student.setCno(99);
+//        student.setDepartment("YourDepartment");
+//        student.setCourse("SE・プログラマ");
+//        student.setMail("40104kk@saisen.ac.jp");
         //ssoから取得する用
         //ssoには、学籍番号、名前が含まれている。
         //apiからのデータには、学年、クラス、出席番号、学科、コースが含まれている。
         //両方のデータをStudentクラスにsetする。
-        //Map<String, String> map = service.getPersonInfo(response, request);
-//        String ssoStudentId = map.get("mellon-email").substring(0, 5);
-//        Student student = service.getStudentInfo(Integer.parseInt(ssoStudentId));
-//        student.setSurname(map.get("mellon-surname"));
-//        student.setGivenname(map.get("mellon-givenname"));
-//        student.setMail(map.get("mellon-email"));
+        Map<String, String> map = service.getPersonInfo(response, request);
+        String ssoStudentId = map.get("mellon-email").substring(0, 5);
+        Student student = service.getStudentInfo(Integer.parseInt(ssoStudentId));
+        student.setSurname(map.get("mellon-surname"));
+        student.setGivenname(map.get("mellon-givenname"));
+        student.setMail(map.get("mellon-email"));
 
 
         //urlからのデータ
